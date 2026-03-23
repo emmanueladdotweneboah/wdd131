8855b4e82333dd3ffa1fa5fb9430febad3269a53
emmanueladdotweneboah <slimaddo06@gmail.com>
2026-03-23 23:21:45 +0000

    done again

--- scripts/place.js
+++ scripts/place.js
@@ -1,21 +1,15 @@
-// FOOTER DATES
-const year = document.getElementById("year");
-const lastModified = document.getElementById("lastModified");
+// FOOTER
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
 
-year.textContent = new Date().getFullYear();
-lastModified.textContent = document.lastModified;
-
-
-// WEATHER VALUES (STATIC)
+// STATIC WEATHER VALUES
 const temp = 28;
 const windSpeed = 10;
 
-
-// WIND CHILL FUNCTION (ONE LINE RETURN)
+// FUNCTION (ONE LINE RETURN ✅)
 function calculateWindChill(t, v) {
     return (13.12 + 0.6215 * t - 11.37 * Math.pow(v, 0.16) + 0.3965 * t * Math.pow(v, 0.16)).toFixed(2);
 }
-
 
 // DISPLAY WIND CHILL
 const windChill = document.getElementById("windChill");
