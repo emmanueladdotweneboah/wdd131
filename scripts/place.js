a1322d53fbdda92e3380302e28e15f91ae2bccf6
emmanueladdotweneboah <slimaddo06@gmail.com>
2026-03-23 23:13:54 +0000

    week 3

---
+++ scripts/place.js
@@ -0,0 +1,27 @@
+// FOOTER DATES
+const year = document.getElementById("year");
+const lastModified = document.getElementById("lastModified");
+
+year.textContent = new Date().getFullYear();
+lastModified.textContent = document.lastModified;
+
+
+// WEATHER VALUES (STATIC)
+const temp = 28;
+const windSpeed = 10;
+
+
+// WIND CHILL FUNCTION (ONE LINE RETURN)
+function calculateWindChill(t, v) {
+    return (13.12 + 0.6215 * t - 11.37 * Math.pow(v, 0.16) + 0.3965 * t * Math.pow(v, 0.16)).toFixed(2);
+}
+
+
+// DISPLAY WIND CHILL
+const windChill = document.getElementById("windChill");
+
+if (temp <= 10 && windSpeed > 4.8) {
+    windChill.textContent = calculateWindChill(temp, windSpeed) + " °C";
+} else {
+    windChill.textContent = "N/A";
+}
